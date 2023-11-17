00d4062cb801b4eb92825839668a9ddec72e002a
ryan <ryan.nascimento257@gmail.com>
2023-11-17 09:08:42 -0300

    commit

--- edit.php
+++ edit.php
@@ -3,21 +3,29 @@
 ?>
   <div class="container">
     <?php include_once("templates/backbtn.html"); ?>
-    <h1 id="main-title">Editar contato</h1>
+    <h1 id="main-title">Editar Cliente</h1>
     <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
       <input type="hidden" name="type" value="edit">
-      <input type="hidden" name="id" value="<?= $contact['id'] ?>">
+      <input type="hidden" name="CodCliente" value="<?= $clientes['CodCliente'] ?>">
       <div class="form-group">
-        <label for="name">Nome do contato:</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome" value="<?= $contact['name'] ?>" required>
+        <label for="Nome">Nome do Cliente:</label>
+        <input type="text" class="form-control" id="Nome" name="Nome" placeholder="Digite o nome" value="<?= $clientes['Nome'] ?>" required>
       </div>
       <div class="form-group">
-        <label for="phone">Telefone do contato:</label>
-        <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o telefone" value="<?= $contact['phone'] ?>" required>
+        <label for="Endereco">Endereço do Cliente:</label>
+        <input type="text" class="form-control" id="Endereco" name="Endereco" placeholder="Digite o endereço" value="<?= $clientes['Endereco'] ?>" required>
       </div>
       <div class="form-group">
-        <label for="observations">Observações:</label>
-        <textarea type="text" class="form-control" id="observations" name="observations" placeholder="Insira as observações" rows="3"><?= $contact['observations'] ?></textarea>
+        <label for="Telefone">Telefone do Cliente:</label>
+        <input type="text" class="form-control" id="Telefone" name="Telefone" placeholder="Digite o telefone" value="<?= $clientes['Telefone'] ?>" required>
+      </div>
+      <div class="form-group">
+        <label for="Celular">Celular do Cliente:</label>
+        <input type="text" class="form-control" id="Celular" name="Celular" placeholder="Digite o telefone" value="<?= $clientes['Celular'] ?>" required>
+      </div>
+      <div class="form-group">
+        <label for="CPF">CPF do Cliente:</label>
+        <input type="text" class="form-control" id="CPF" name="CPF" placeholder="Digite o CPF" value="<?= $clientes['CPF'] ?>" required>
       </div>
       <button type="submit" class="btn btn-primary">Atualizar</button>
     </form>
